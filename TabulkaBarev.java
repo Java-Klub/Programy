6761ae922b23ee2703cdd1549b2c89c750b36d50
Adam Sotona <adam.sotona@oracle.com>
2024-10-04 12:59:00 +0200

    odtraneny priklady urcene az pro dalsi hodiny

--- TabulkaBarev.java
+++
@@ -1,9 +0,0 @@
-void main() {
-    println("tabulka barev:");
-    for (var barvaPozadi = 40; barvaPozadi < 48; barvaPozadi++) {
-        for (var barvaPisma = 30; barvaPisma < 38; barvaPisma++) {
-            print("\033[" + barvaPisma + ";" + barvaPozadi + "m\\033[" + barvaPisma + ";" + barvaPozadi + "m\033[0m ");
-        }
-        println("");
-    }
-}
